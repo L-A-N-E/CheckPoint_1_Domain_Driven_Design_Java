da6b1ce5b297358cbdb05cc7f26b088355d0a862
Nicolas <haubrichtnicolas@gmail.com>
2025-03-19 10:19:10 -0300

    Alterando Codigo

--- src/sistema_estacionamento/Sistema_Estacionamento.java
+++ src/sistema_estacionamento/Sistema_Estacionamento.java
@@ -6,12 +6,13 @@ public class Sistema_Estacionamento {
 
 //      Criando Scanner
         Scanner input = new Scanner(System.in); 
-        
+
 //      Declarando variáveis do tipo inteiras
         int opcao_menu, horario_entrada, horario_saida, horario_final, valor_estacionamento_por_hora, valor_estacionamento_final;
 
 //      Declarando variáveis do tipo String
-        String marca_veiculo, modelo_veiculo, cor_veiculo, placa_veiculo, vaga_localizao;
+        String marca_veiculo = "", modelo_veiculo, cor_veiculo, placa_veiculo, vaga_localizao, consultar_placa;
+
         
 //      Laço de Repetição que Contém o Menu de Opções
         while(true){
@@ -31,23 +32,44 @@ public class Sistema_Estacionamento {
             switch(opcao_menu){
 //              Opção 1 - Cadastrar Veículo
                 case 1:
-                    System.out.println("#== CADASTRAR VEICULO NOVO ==#");
-                    System.out.println("Digite a marca do veiculo");
+                    if (marca_veiculo.isEmpty()) {
+                        System.out.println("#== CADASTRAR VEICULO NOVO ==#");
+                    } else {
+                        System.out.println("#== SOBREESCREVER VEICULO ==#");
+                    }
+
+                    System.out.println("Digite a marca do veículo:");
                     marca_veiculo = input.next();
-                    System.out.println("Digite o modelo do veiculo");
+
+                    System.out.println("Digite o modelo do veículo:");
                     modelo_veiculo = input.next();
-                    System.out.println("Digite a cor do veiculo");
+
+                    System.out.println("Digite a cor do veículo:");
                     cor_veiculo = input.next();
-                    System.out.println("Digite a placa do veiculo");
+                    
+                    System.out.println("Digite a placa do veículo:");
                     placa_veiculo = input.next();
-                    System.out.println("Digite a localizacao da vaga");
+                    
+                    System.out.println("Digite o horário de entrada (somente números inteiros):");
+                    horario_entrada = input.nextInt();
+
+                    System.out.println("Digite a localização da vaga:");
                     vaga_localizao = input.next();
-                    System.out.println(marca_veiculo + modelo_veiculo + cor_veiculo + placa_veiculo + vaga_localizao);
+
+                    System.out.println("\n### Dados do Veículo ###");
+                    System.out.println("Marca: " + marca_veiculo);
+                    System.out.println("Modelo: " + modelo_veiculo);
+                    System.out.println("Cor: " + cor_veiculo);
+                    System.out.println("Placa: " + placa_veiculo);
+                    System.out.println("Horário de Entrada: " + horario_entrada + ":00");
+                    System.out.println("Localização da Vaga: " + vaga_localizao);
                     break;
 
-//              Opção 2 - Consultar Veículo
+//              Opção 2 - Retirar Veículo
                 case 2:
+                    System.out.println("#== REMOVER VEICULO DO PÁTIO ==#");
                     System.out.println("Digite a placa do veiculo");
+                    consultar_placa = input.next();
                     break;
 
 //              Opção 3 - Encerrar Programa                    
